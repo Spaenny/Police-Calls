e490300a83bfc82bce003fe4db4aceefa1ab4ed5
Manuel <manuel@sunred.org>
2015-04-03 20:56:03 +0200

    Fix possible errors

--- lua/PoliceCall/sh_policecalls.lua
+++ lua/PoliceCall/sh_policecalls.lua
@@ -4,8 +4,9 @@ PPC.defaultLanguage = "english"
 PPC.lang = {}
 PPC.defl = {}
 
-PPC:LoadLanguage(PPC.language)
-PPC:LoadLanguage(PPC.defaultLanguage)
+if PPC:LoadLanguage(PPC:GetLanguage()) and PPC:GetLanguage() ~= PPC.defaultLanguage then
+	PPC:LoadLanguage(PPC.defaultLanguage)
+end
 
 function PPC:GetLanguage()
 	return self.Language or self.defaultLanguage
@@ -34,21 +35,25 @@ function PPC:LoadLanguage(name)
 			else
 				self.defl[name] = temp.t
 			end
+			return true
 		else
 			MsgC(Color(255, 50, 50), "Loading " .. name .. " translation failed\nError: " .. err .. "\n")
+			return false
 		end
 	else
 		MsgC(Color(255, 50, 50), "Failed to compile language file 'PoliceCall/lang/" .. name .. ".lua'\n")
+		return false
 	end
 end
 
 function PPC:Translate(key, ...)
 	local args = {...}
 	if self.lang[key] then
-		return string.format(self.lang[key], unpack(args)) or ""
+		return string.format(self.lang[key], unpack(args)) or "PPC.lang." .. tostring(key)
 	elseif self.defl[key]
-		return string.format(self.lang[key], unpack(args)) or ""
+		return string.format(self.lang[key], unpack(args)) or "PPC.lang." .. tostring(key)
 	else
-		MsgC(Color(255, 50, 50), "'" .. key .. "' is missing in default language table!'\n")
+		MsgC(Color(255, 50, 50), "'" .. tostring(key) .. "' is missing in default language table!'\n")
+		return "PPC.lang." .. tostring(key)
 	end
 end
